b20ff021ce56615c59537b7a5a88846a5ba96c55
ngross144 <60324070+ngross144@users.noreply.github.com>
2022-05-13 00:16:27 -0400

    Add files via upload

---
+++ console.sql
@@ -0,0 +1,59 @@
+USE store;
+
+insert into customers (id, firstname, lastname) values (1, 'Mario', 'Mario');
+insert into customers (id, firstname, lastname) values (2, 'Samus', 'Aran');
+insert into customers (id, firstname, lastname) values (3, 'Captain', 'Falcon');
+
+insert into products (id, product, cost, manufacturer) VALUES (1, 'Armor', 15, 'Metroid');
+insert into products (id, product, cost, manufacturer) VALUES (2, 'Gloves', 5, 'Mushroom Kingdom');
+insert into products (id, product, cost, manufacturer) VALUES (3, 'ship', 15, 'Metroid');
+insert into products (id, product, cost, manufacturer) VALUES (4, 'Overalls', 15, 'Mushroom Kingdom');
+insert into products (id, product, cost, manufacturer) VALUES (5, 'Maxim Tomato', 10, 'Super Smash Bros.');
+insert into products (id, product, cost, manufacturer) VALUES (6, 'Blue Falcon', 15, 'F-Zero');
+insert into products (id, product, cost, manufacturer) VALUES (7, 'stuff', 2, 'people');
+insert into products (id, product, cost, manufacturer) VALUES (8, 'things', 2, 'people');
+insert into products (id, product, cost, manufacturer) VALUES (9, 'objects', 2, 'people');
+insert into products (id, product, cost, manufacturer) VALUES (10, 'junk', 2, 'people');
+
+insert into orders (id, product, cost, customerId) VALUES (1, 'Armor', 15, 2);
+insert into orders (id, product, cost, customerId) VALUES (2, 'Gloves', 5, 1);
+insert into orders (id, product, cost, customerId) VALUES (3, 'ship', 20, 2);
+insert into orders (id, product, cost, customerId) VALUES (4, 'overalls', 10, 1);
+insert into orders (id, product, cost, customerId) VALUES (5, 'Maxim Tomato', 10, 1);
+insert into orders (id, product, cost, customerId) VALUES (6, 'Maxim Tomato', 10, 2);
+insert into orders (id, product, cost, customerId) VALUES (7, 'Blue Falcon', 15, 3);
+
+insert into product_customer_association (id, product_id, customer_id) VALUES (1, 1, 2);
+insert into product_customer_association (id, product_id, customer_id) VALUES (2, 2, 1);
+insert into product_customer_association (id, product_id, customer_id) VALUES (3, 3, 2);
+insert into product_customer_association (id, product_id, customer_id) VALUES (4, 4, 1);
+insert into product_customer_association (id, product_id, customer_id) VALUES (5, 5, 1);
+insert into product_customer_association (id, product_id, customer_id) VALUES (6, 5, 2);
+insert into product_customer_association (id, product_id, customer_id) VALUES (7, 6, 3);
+
+select * from customers;
+select * from orders;
+select * from product_customer_association;
+select * from products;
+
+select product from orders;
+select cost, manufacturer from products;
+select product_id, customer_id from product_customer_association;
+select lastname from customers;
+
+delete from products where id = 7;
+delete from products where id in (8, 9, 10);
+
+select orders.product, orders.cost, customers.firstname
+from customers
+INNER JOIN orders on customers.id = orders.customerId;
+
+select orders.product, orders.cost, customers.firstname
+from customers
+INNER JOIN orders on customers.id = orders.customerId
+where firstname = 'Mario';
+
+select orders.product, orders.cost, customers.firstname
+from customers
+INNER JOIN orders on customers.id = orders.customerId
+where cost = 15;
